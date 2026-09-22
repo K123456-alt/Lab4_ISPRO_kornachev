3a6440146a731b77eb67fd371563dd6813ccd8e7
K123456-alt <kornachevkirill2008@gmail.com>
2026-09-22 14:42:00 +0300

    add folders data and logs

--- server/program.cs
+++ server/program.cs
@@ -2,18 +2,18 @@
 
 {
     static void Main()
-{
-Console.WriteLine("Здравсвуйте!");
-Console.WriteLine("Корначев Кирилл Александрович");
-Console.WriteLine("Группа: ИСП-243");
-Console.WriteLine("Дата: 22.09.2026");
-Console.WriteLine("1 - Показать ФИО");
-Console.WriteLine("2 - Показать группу");
-Console.WriteLine("3 - Показать дату");
-Console.WriteLine("4 - Выход");
-Console.Write("Выберите пункт: ");
+    {
+        Console.WriteLine("Здравсвуйте!");
+        Console.WriteLine("Корначев Кирилл Александрович");
+        Console.WriteLine("Группа: ИСП-243");
+        Console.WriteLine("Дата: 22.09.2026");
+        Console.WriteLine("1 - Показать ФИО");
+        Console.WriteLine("2 - Показать группу");
+        Console.WriteLine("3 - Показать дату");
+        Console.WriteLine("4 - Выход");
+        Console.Write("Выберите пункт: ");
 
-string choice = Console.ReadLine();
+        string choice = Console.ReadLine();
 
         if (choice == "1")
         {
