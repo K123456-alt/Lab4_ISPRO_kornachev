92f51bf13a37ecafbcae7b9c2d9c7dafcf420734
K123456-alt <kornachevkirill2008@gmail.com>
2026-09-22 13:27:28 +0300

    add Logika Vibora

--- server/program.cs
+++ server/program.cs
@@ -1,8 +1,8 @@
-﻿// class Program
+﻿class Program
 
-// {
-//     static void Main()
-// {
+{
+    static void Main()
+{
 Console.WriteLine("Здравсвуйте!");
 Console.WriteLine("Корначев Кирилл Александрович");
 Console.WriteLine("Группа: ИСП-243");
@@ -12,4 +12,31 @@ Console.WriteLine("2 - Показать группу");
 Console.WriteLine("3 - Показать дату");
 Console.WriteLine("4 - Выход");
 Console.Write("Выберите пункт: ");
+
+string choice = Console.ReadLine();
+
+        if (choice == "1")
+        {
+            Console.WriteLine("Корначев Кирилл Александрович");
+        }
+        else if (choice == "2")
+        {
+            Console.WriteLine("ИСП-243");
+        }
+        else if (choice == "3")
+        {
+            Console.WriteLine("Дата: 22.09.2026");
+        }
+        else if (choice == "4")
+        {
+            Console.WriteLine("Пока");
+        }
+        else
+        {
+            Console.WriteLine("Неверный пункт");
+        }
+
+        Console.ReadLine();
+    }
+}
 
