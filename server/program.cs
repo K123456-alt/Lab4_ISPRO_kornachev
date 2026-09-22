291725dd494b65b7bdeced0a84c21f5a907b0650
K123456-alt <kornachevkirill2008@gmail.com>
2026-09-22 13:26:49 +0300

    ADD MAIN pravilniy variant

--- server/program.cs
+++ server/program.cs
@@ -1,5 +1,15 @@
-﻿Console.WriteLine("Здравсвуйте!");
+﻿// class Program
+
+// {
+//     static void Main()
+// {
+Console.WriteLine("Здравсвуйте!");
 Console.WriteLine("Корначев Кирилл Александрович");
 Console.WriteLine("Группа: ИСП-243");
-Console.WriteLine("Дата: 22.09.2026")
+Console.WriteLine("Дата: 22.09.2026");
+Console.WriteLine("1 - Показать ФИО");
+Console.WriteLine("2 - Показать группу");
+Console.WriteLine("3 - Показать дату");
+Console.WriteLine("4 - Выход");
+Console.Write("Выберите пункт: ");
 
