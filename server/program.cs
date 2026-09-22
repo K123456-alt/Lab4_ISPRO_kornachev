1233ae82907a31c6a0f663d8a4f6d8584f3b737b
K123456-alt <kornachevkirill2008@gmail.com>
2026-09-22 13:10:50 +0300

    add group and date

--- server/program.cs
+++ server/program.cs
@@ -1,3 +1,5 @@
 ﻿Console.WriteLine("Здравсвуйте!");
-Console.WriteLine("Корначев Кирилл Александрович")
+Console.WriteLine("Корначев Кирилл Александрович");
+Console.WriteLine("Группа: ИСП-243");
+Console.WriteLine("Дата: 22.09.2026")
 
